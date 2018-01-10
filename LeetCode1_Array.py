8334291c312cb2dc7463c3945331587085a97e00
chenjian <chenjian@kankan.com>
2018-01-10 14:24:13 +0800

    数组系列问题，3sum等

--- LeetCode1_Array.py
+++ LeetCode1_Array.py
@@ -1,9 +1,12 @@
-# ！Given a sorted array, remove the duplicates in place such that each element appear only once
-# ！and return the new length.
-# ！Do not allocate extra space for another array, you must do this in place with constant memory.
-# ！For example, Given input array A = [1,1,2],
-# ！Your function should return length = 2, and A is now [1,2].
+# Given a sorted array, remove the duplicates in place such that each element appear only once
+# and return the new length.
+# Do not allocate extra space for another array, you must do this in place with constant memory.
+# For example, Given input array A = [1,1,2],
+# Your function should return length = 2, and A is now [1,2].
 from re import L
+
+import sys
+from numpy import sort
 
 
 def remove_duplicates(L=[]):
@@ -189,10 +192,105 @@ def get_sort_length(A=[]):
 # You may assume that each input would have exactly one solution.
 # Input: numbers={2, 7, 11, 15}, target=9
 # Output: index1=1, index2=2
+# 需要返回index值，所以
+# 1、如果用双重for循环的话 时间复杂度是O(n~2) 会超时
+# 2、先排序 然后二分 找到 小于 9的位置 但是要返回index下标，所以也不能进行排序
+# 3、用一个dict  存储每个值的下标index
 #
-def twoSum(A=[]):
-    pass
+def two_sum(A=[], target=0):
+    d = {}
+    for i in range(len(A)):
+        d[A[i]] = i
+    for z in range(len(A)):
+        gap = target - A[z]  # 算出和A[i] 匹配的数字的值，然后去map中寻找
+        if d.get(gap) is not None and d[gap] > z:  # 因为是从0开始的，所以 后面的数字只用判断更后面的即可，所以加上>z的判定
+            print('第一个值的index= %s，第二个字的index= %s' % (z, d[gap]))
 
 
+# Given an array S of n integers, are there elements a; b; c in S such that a + b + c = 0? Find all unique
+# triplets in the array which gives the sum of zero.
+# Note:
+# Elements in a triplet (a; b; c) must be in non-descending order. (ie, a  b  c)
 
-print(get_sort_length([3, 5, 8, 7, 10, 0, 6, 1, 9, 2]))
+# The solution set must not contain duplicate triplets.
+# For example, given array S = {-1 0 1 2 -1 -4}.
+# A solution set is:
+# (-1, 0, 1)
+# (-1, -1, 2)
+# 从一个数组中找到 三个数 a，b，c ，并且 a + b + c = target
+# 输出结果升序排列
+# 输出结果没有重复的
+#
+# 我们需要注意的是，这个数据集 是无序的 而且有可能包含重复的值
+# 输出的也并不是index而是值本身
+# 所以，我们可以对源数据 先进行排序 快速排序的时间复杂度是O（nlgn）
+# 然后先固定一个数 再找到其他符合标准的两个数
+# 需要注意的有两点
+# 1、如果固定的那个数的下一个数是重复的，那么需要跳过下一个数，不然会出现重复的式子
+# 2、如果 x + y + z = target 的时候，j和k同时移动
+# 3、 在j和k同时移动的时候 要判断 他们的下一个值是否和上一个值相同 如果都相同 说明是重复的数字
+# 时间复杂度  貌似是O(n * n)
+def three_sum(A=[], target=0):
+    B = sort(A)
+    print(B)
+    for i in range(len(B) - 2):
+        # 先固定一个数 B[i]
+        if i > 0 and B[i] == B[i - 1]:
+            i += 1
+            continue
+        x = B[i]
+        j = i + 1
+        k = len(B) - 1
+        print('外层循环---::%s' % x)
+        while j < k:
+            if x + B[j] + B[k] < target:
+                j += 1
+                while x + B[j] + B[k] < target:
+                    j += 1
+            elif x + B[j] + B[k] > target:
+                k -= 1
+                while x + B[j] + B[k] > target:
+                    k -= 1
+            else:
+                print('满足要求的输出结果为  %s  %s  %s' % (x, B[j], B[k]))
+                j += 1
+                k -= 1
+                # 当有满足要求的式子的时候 需要跳过附近重复的数字
+                while B[j] == B[j - 1] and B[k] == B[k + 1] and j < k:
+                    j += 1
+                    k -= 1
+
+
+# Given an array S of n integers, find three integers in S such that the sum is closest to a given number,
+# target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+# For example, given array S = {-1 2 1 -4}, and target = 1.
+# The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+# 跟上面那道题很像 但是不同的是 不再判断是否 = target 而是记录 和target的差最小的
+# 其实道理都是一样的
+
+def three_sum_closest(A=[], target=0):
+    B = sort(A)
+    print(B)
+    value = 100
+    ss = ''
+    for i in range(len(B) - 2):
+        x = B[i]
+        if i > 0 and B[i] == B[i - 1]:
+            continue
+        j = i + 1
+        k = len(B) - 1
+        while j < k:
+            result = abs((x + B[j] + B[k]) - target)
+            # 计算出当前的差值
+            print('resulet %s and value %s' % (result,value))
+            if result < value:
+                ss = '当前记录的大小 %s  %s %s ' % (x, B[j], B[k])
+                value = result
+            if x + B[j] + B[k] < target:
+                j += 1
+            else:
+                k -= 1
+
+    print(ss)
+
+print(three_sum_closest([-1, 2, 1, -4], 1))
