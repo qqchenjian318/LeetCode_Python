24bc2c8d0fe6e52759d994c7bb28bfb4bbc498d0
chenjian <chenjian@kankan.com>
2018-02-02 16:58:44 +0800

    完成数组的相关题目

--- LeetCode2_Array.py
+++ LeetCode2_Array.py
@@ -167,6 +167,7 @@ def plus_one(L=[], a=1):
         L.insert(s, 1)
     return L
 
+
 # Climbing Stairs
 # You are climbing a stair case. It takes n steps to reach to the top.
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
@@ -184,6 +185,7 @@ def climbing_stairs(n=0):
     if n == 2:
         return 2
     return climbing_stairs(n - 1) + climbing_stairs(n - 2)
+
 
 # 其实这道题 如果用遍历的方式也可以解决的
 # 其实就是一个斐波那契数列，因为f(n) = f(n - 1) + f(n - 2)
@@ -276,4 +278,197 @@ def binary_to_gray(i=0):
 # 如果使用的是O(m + n)大小的内存，就有一些进步了，但是依然不是最好的解法
 # 你能否实现一个使用常数 大小的内存空间的解法呢？
 
-print(gray_code(3))
+def matrix_zeroes(L=[]):
+    m = len(L)
+    n = len(L[0])
+    print(L)
+
+    row_has = False
+    column_has = False
+    # 说明行有0
+    for i in range(m):
+        if L[i][0] == 0:
+            row_has = True
+            break
+    # 说明竖有0
+    for j in range(n):
+        if L[0][j] == 0:
+            column_has = True
+            break
+
+    # 遍历其他行列  如果有0 就将该数组的值设为0
+    for i in range(m):
+        for j in range(n):
+            if L[i][j] == 0:
+                L[i][0] = 0
+                L[0][j] = 0
+
+    # 分别数组 如果第一行和第一列对应位置为0 那么设置为0
+    for i in range(m):
+        for j in range(n):
+            if L[i][0] == 0 or L[0][j] == 0:
+                L[i][j] = 0
+    if row_has:
+        for i in range(m):
+            L[i][0] = 0
+    if column_has:
+        for j in range(n):
+            L[0][j] = 0
+
+    print(L)
+
+
+# Gas Station
+# There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+# You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next
+# station (i+1). You begin the journey with an empty tank at one of the gas stations.
+# Return the starting gas station’s index if you can travel around the circuit once, otherwise return -1.
+# Note: The solution is guaranteed to be unique.
+#
+# 沿途有n个加气站 ，每个站拥有的天然气数量是 gas[i]，你有一辆 无限大气罐的车，到下一个加气站(i + 1)需要花费的天然气 是 costs[i]
+# 你从目前是空车 并从任意一个加气站出发
+# 如果能够环绕一圈 就返回你出发的加气站的index ，否则的话 返回 -1
+#
+# 额。。。这道题呢 主要就是一个关键点 消耗的天然气和加油站拥有的天然气的差值
+# 比如可以从差值最大的地方出发
+#
+# 每个站点的结余可以看着 diff[i] = gas[i] - cost[i]
+# 那么如果从m点出发 到达p点 车里剩余的天然气数量是 diff[m] + diff[m+1]+....+diff[p] = sum1 ，如果sum1 < 0 ，说明从m点出发不能环绕一圈
+# 那么我们从 p+1点出发，直到n点 剩余的天然气数量是 diff[p+1]+diff[p+1+1]+....+diff[n] = sum2，如果sum2 <0 ，说明还是不能成功环绕一圈
+# 那么我们从 n+1点出发，知道m点，剩余的天然气数量是 diff[n+1] + diff[n+1+1]+...+diff[m] = sum3 ，如果sum3 > 0 ,说明从n+1点到m点，还能有剩余的天然气
+# 那么依次类推 如果 sum1 + sum2 +...+sumN > 0 说明就可以环绕一圈
+# 那如果不行的话 就不可以
+# 所以
+
+def gas_station(gas=[], cost=[]):
+    total = 0
+    sum1 = 0
+    index = 0
+    i = 0
+    while i < len(gas):
+        total += gas[index] - cost[index]
+        sum1 += gas[index] - cost[index]
+        # 当前的所有值 如果total 小于0 那么就重置sum 并记下标记点
+        if sum1 < 0:
+            sum1 = 0
+            index = i
+
+    if total > 0:
+        return index + 1
+    return -1
+
+
+# Candy
+# There are N children standing in a line. Each child is assigned a rating value.
+# You are giving candies to these children subjected to the following requirements:
+# • Each child must have at least one candy.
+# • Children with a higher rating get more candies than their neighbors.
+# What is the minimum candies you must give?
+#
+# 这里有n个小孩子站在一条线上，每个都有一个评分值
+# 你按照下面的要求 给这些小孩子分糖果
+# 每个孩子至少有一个糖果
+# 如果孩子的评分值越高 他获得的糖果就比他临近的人更多
+# 请问  至少需要多少个糖果
+#
+# 判断两个分值大小
+# 如果A[i+1] > A[i] 那么 A[i+1]的糖果最好的数量就是 i位置的糖果数量 + 1
+# 那如果A[i+1] <= A[i] 那应该给多少糖果呢？好像不太能确定到底该给多少糖果 因为我们需要给尽可能的少
+#
+# 我们的思路就可以 首先遍历一遍 大的话 就+1
+# 如果小的话 我们就 不知道应该填入多少了
+# 如果遇到 1 5 4 3 2 这样的数列
+# 首先 1孩子 给1个糖果
+# 5孩子给 2个糖果的话
+# 那么 4孩子 只能给一个糖果
+# 那么3孩子就不能给了 所以就有问题
+# 说明5孩子应该给的比预计中的还要多
+# 而这个5孩子给的数量  其实跟 5 4 3 2 这个递减数列的长度有关系
+# 如果递减数列 长度为4  那么第一个值至少也要为4
+# 所以 我们可以通过引入一个修正值的方式 来实现一次遍历 就计算出total数
+# 修正值的作用就是
+
+# 思路：
+# 首先 从左往右遍历 确保 右边比左边高的小朋友的糖果数比左边多
+# 然后 从右往左遍历 使得 左边比右边得分高的小朋友 糖果数比右边多
+#
+# 例如 1 5 4 3 2
+# 第一次遍历
+# 1 2 0 0 0
+# 第二次遍历
+# 1 4 3 2 1
+
+# 例如 1 5 4 2 3
+# 第一次遍历
+# 1 2 0 0 1
+# 第二次
+# 1 3 2 1 2
+
+# 当然 题目要求 只需要total 也就是最少的糖果数量
+# 并不需要知道每个孩子具体分多少个
+# 所以 其实还有更加节省空间和时间的方法
+# 这里就不多说了
+#
+def candy(L=[]):
+    result = list(range(len(L)))
+    print(L)
+    for i in range(len(L)):
+        if i == 0:
+            result[i] = 1
+            continue
+        if L[i] > L[i - 1]:
+            if result[i - 1] == 0:
+                result[i - 1] = 1
+            result[i] = result[i - 1] + 1
+        else:
+            result[i] = 0
+
+    j = len(L) - 1
+    while j > 0:
+        if j == len(L) - 1 and result[j] == 0:
+            result[j] = 1
+            j -= 1
+            continue
+        if L[j] > L[j + 1]:
+            if result[j] <= result[j+1]:
+                result[j] = result[j+1] + 1
+        j -= 1
+    print(result)
+
+
+# Single Number
+# Given an array of integers, every element appears twice except for one. Find that single one.
+# Note: Your algorithm should have a linear runtime complexity. Could you implement it without using
+# extra memory?
+#
+# 一个数异或同一个数两次的话 就是他本身
+# 所以 我们可以根据异或的特点 写出时间复杂度O(n)和空间复杂度O(1)的算法
+
+def single_num(L=[]):
+    result = 0
+    for i in range(len(L)):
+        result = result ^ L[i]
+
+    return result
+
+# Single Number II
+# Given an array of integers, every element appears three times except for one. Find that single one.
+# Note: Your algorithm should have a linear runtime complexity. Could you implement it without using
+# extra memory?
+#
+# 有一个int数组，除了某一个元素之外，每个元素都出现了三次 ，找出只出现了一次的这个，而且要求不能使用额外的内存空间
+# 这个是上面那个题的变种
+
+# ~按位取反
+# ^ 异或
+
+def single_sum_2(L=[]):
+    a = 0
+    b = 0
+    for i in range(len(L)):
+        b = (b ^ L[i]) & ~a
+        a = (a ^ L[i]) & ~b
+
+    return b
+
+print(single_sum_2([1, 5, 1, 1, 2, 2, 2]))
