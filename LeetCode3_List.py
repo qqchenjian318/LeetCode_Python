ed886ef68ecaa78a7851d7ba7a8156c3c1948ecc
chenjian <chenjian@kankan.com>
2018-02-06 17:02:12 +0800

    开始刷链表的相关题目

---
+++ LeetCode3_List.py
@@ -0,0 +1,10 @@
+
+# LeetCode里面的链表相关的算法题
+
+
+# Add Two Numbers
+# You are given two linked lists representing two non-negative numbers. The digits are stored in reverse
+# order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# 有两个装非负数的链表
