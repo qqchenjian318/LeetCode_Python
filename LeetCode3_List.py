a3e3b6cd4b86dca60facf615ed145fabdfbdcba5
chenjian <chenjian@kankan.com>
2018-02-28 20:56:49 +0800

    链表 reverse linked

--- LeetCode3_List.py
+++ LeetCode3_List.py
@@ -59,10 +59,8 @@ b = ListNode(0)
 c = ListNode(0)
 a.val = 2
 a.next = b
-
 b.val = 4
 b.next = c
-
 c.val = 3
 c.next = None
 
@@ -76,8 +74,56 @@ m.next = c
 n.val = 4
 n.next = None
 
-z = two_number_add(a, l)
 
-while z is not None:
-    print('%s  ' % z.val)
-    z = z.next
+# Reverse Linked List II
+# Reverse a linked list from position m to n. Do it in-place and in one-pass.
+# For example: Given 1->2->3->4->5->nullptr, m = 2 and n = 4,
+# return 1->4->3->2->5->nullptr.
+# Note: Given m, n satisfy the following condition: 1 <= m <= n <= length of list
+#
+# 这个题就是reverse一个链表从 值m 到 值n，在原链上一次完成
+#
+# 思路就是从时候
+#
+def reverse_link(a , m, n):
+    temp2 = None
+    new = ListNode(0)
+    new.val = 'start'
+    new.next = a
+    for i in range(m - 1):
+        a = a.next
+    newLink = a  # newLink 现在是2
+    # 从new 开始 就是一个全新的链表
+    while m <= n:
+        m += 1
+        # 此时的a 其实就是第m个节点
+        temp = a.next
+        a.next = temp2
+        temp2 = a
+        a = temp
+    newLink.next = a
+    new.next.next = temp2
+    return new
+
+
+a = ListNode(0)
+b = ListNode(0)
+c = ListNode(0)
+d = ListNode(0)
+e = ListNode(0)
+f = ListNode(0)
+
+a.val = 1
+a.next = b
+b.val = 2
+b.next = c
+c.val = 3
+c.next = d
+d.val = 4
+d.next = e
+e.val = 5
+e.next = None
+result = reverse_link(a, 2, 5)
+while result is not None:
+    print(result.val)
+    result = result.next
